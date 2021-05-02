0535a3fa3735c5879ea58aaa1ad92a0f13b33767
Lukasz Klekner <lukaszklekner@gmail.com>
2021-05-02 16:15:19 +0200

    added styled-components into SocialButton component

--- src/components/SocialButton.js
+++ src/components/SocialButton.js
@@ -1,10 +1,35 @@
+import styled from 'styled-components';
+
 const SocialButton = ({src, alt, text}) => (
-    <div className="social">
-        <a className="social-link" href="#">
-            <img className="social-image" src={src} alt={alt} />
-            <span className="social-text">Rejestracja przez {text}</span>
-        </a>
-    </div>
+    <Social>
+        <SocialLink href="#">
+            <img src={src} alt={alt} />
+            <SocialText>Rejestracja przez {text}</SocialText>
+        </SocialLink>
+    </Social>
 )
 
 export default SocialButton;
+
+const Social = styled.div`
+    width: 85%;
+    padding: 10px;
+    border: 1px solid #413fb5;
+    border-radius: 5px;
+    font-size: 1.05rem;
+    font-weight: 500;
+    padding: 10px;
+    margin-bottom: 10px;
+`
+
+const SocialLink = styled.a`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    text-decoration: none;
+    color: black;
+`
+
+ const SocialText = styled.span`
+    margin-left: 15px;
+`
