71ee9a7e938d093f616c1c692d0039ecb15a21e1
Lukasz Klekner <lukaszklekner@gmail.com>
2021-05-02 14:26:49 +0200

    added JSX structure for Form Component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,16 @@
 const Form = () => (
     <form action="#" method="POST">
-        DZIALA
+        <div className="personal-details">
+            <input className="input" type="text" name="name" placeholder="First name..." />
+            <input className="input" type="text" name="surname" placeholder="Last name..." />
+        </div>
+        <input className="full-input" type="email" name="email" placeholder="Email address..." />
+        <input className="full-input" type="password" name="password" placeholder="Password..." />
+        <label className="label">
+            <input className="agreement" type="checkbox" name="agree" />
+            <span>I have read the <a className="special" href="#">Terms & Conditions</a></span>
+        </label>
+        <button className="button">Zarejestruj się za darmo</button>
     </form>
 )
 
--- src/components/SocialButton.js
+++ src/components/SocialButton.js
@@ -1,6 +1,6 @@
 const SocialButton = ({src, alt, text}) => (
     <div className="social">
-        <a class="social-link" href="#">
+        <a className="social-link" href="#">
             <img className="social-image" src={src} alt={alt} />
             <span className="social-text">Rejestracja przez {text}</span>
         </a>
