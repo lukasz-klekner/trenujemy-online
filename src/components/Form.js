a9d72e6c2f2576257933ef3d7e925679aa4a9bde
Lukasz Klekner <lukaszklekner@gmail.com>
2021-05-02 17:19:35 +0200

    added styled-components into Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styled from 'styled-components';
 
 const Form = () => {
     const [name, setName] = useState("");
@@ -15,53 +16,96 @@ const Form = () => {
 
 
     return (
-    <form action="#" method="POST">
-        <div className="personal-details">
-            <input
-                className="input"
+    <FitnessForm action="#" method="POST">
+        <PersonalDetails>
+            <Input
                 type="text" name="name"
                 placeholder="First name..."
                 value={name}
                 onChange={handleChangeName}
             />
-            <input
-                className="input"
+            <Input
                 type="text"
                 name="surname"
                 placeholder="Last name..."
                 value={surname}
                 onChange={handleChangeSurname}
             />
-        </div>
-        <input
-            className="full-input"
+        </PersonalDetails>
+        <FullInput
             type="email"
             name="email"
             placeholder="Email address..."
             value={emial}
             onChange={handleChangeEmail}
             />
-        <input
-            className="full-input"
+        <FullInput
             type="password"
             name="password"
             placeholder="Password..."
             value={password}
             onChange={handleChangePassword}
             />
-        <label className="label">
-            <input
-                className="agreement"
+        <Label>
+            <Agreement
                 type="checkbox"
                 name="agree"
                 checked={aggrement}
                 onChange={handleChangeAgreement}
             />
-            <span>I have read the <a className="special" href="#">Terms & Conditions</a></span>
-        </label>
-        <button className="button">Zarejestruj się za darmo</button>
-    </form>
+            <span>I have read the <Special href="#">Terms & Conditions</Special></span>
+        </Label>
+        <Button>Zarejestruj się za darmo</Button>
+    </FitnessForm>
 )
 }
 
 export default Form;
+
+const FitnessForm = styled.form`
+    width: 85%;
+    height: 40vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+`
+
+const PersonalDetails = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const FullInput = styled.input`
+    padding: 10px;
+    padding-left: 20px;
+    border-radius: 8px;
+    border: 1px solid silver;
+`
+
+const Input = styled(FullInput)`
+    width: 38%;
+`
+const Label = styled.label`
+    display: flex;
+    align-items: center;
+`
+
+const Agreement = styled.input`
+    margin-right: 10px;
+`
+
+const Special = styled.a`
+      border-bottom: 2px solid #413fb5;
+    text-decoration: none;
+    color: black;
+    font-weight: 600;
+`
+
+const Button = styled.button`
+    font-size: 1.15rem;
+    padding: 10px;
+    border-radius: 10px;
+    background-color: #413fb5;
+    border: 1px solid #413fb5;
+    color: white;
+`
