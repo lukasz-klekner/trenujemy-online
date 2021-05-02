c662beb027e2421e28a00f05879cb71b5a5765d9
Lukasz Klekner <lukaszklekner@gmail.com>
2021-05-02 16:09:07 +0200

    added styled-components into App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,112 @@
 import logo from '../assets/images/logo.png';
 import fb from '../assets/images/fbIcon.png';
 import instagram from '../assets/images/instaIcon.png';
+import gym from '../assets/images/gym.jpg'
+
+import styled from 'styled-components';
 
 import SocialButton from './SocialButton';
 import Form from './Form';
 
 const App = () => (
-  <main>
-    <img src={logo} alt="Logo"/>
-    <div className="wrapper">
-      <div className="registration">
-        <h2>Zarejestruj się jako trener</h2>
-        <SocialButton src={fb} alt="Facebook icon" text="Facebook"/>
-        <SocialButton src={instagram} alt="Instagram icon" text="Instagram"/>
-        <div className="divider">
-          <div className="line"></div>
-          <span>Lub za pomocą email</span>
-          <div className="line"></div>
-        </div>
-        <Form />
-        <div className="footer"> Masz już konto? <a className="special" href="#">Zaloguj się</a></div>
-      </div>
-      <div className="main-image">Image</div>
-    </div>
-  </main>
+  <Body>
+    <Main>
+      <img src={logo} alt="Logo"/>
+      <Wrapper>
+        <Registration>
+          <Title>Zarejestruj się jako trener</Title>
+          <SocialButton src={fb} alt="Facebook icon" text="Facebook"/>
+          <SocialButton src={instagram} alt="Instagram icon" text="Instagram"/>
+          <Divider>
+            <Line></Line>
+            <span>Lub za pomocą email</span>
+            <Line></Line>
+          </Divider>
+          <Form />
+          <Footer> Masz już konto? <Special href="#">Zaloguj się</Special></Footer>
+        </Registration>
+        <MainImage></MainImage>
+      </Wrapper>
+    </Main>
+  </Body>
 )
 
 export default App;
+
+const Body = styled.div`
+  font-family: system-ui;
+  font-size: 10px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Main =  styled.main`
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 50vw;
+  border: 1px solid black;
+  border-radius: 20px;
+  margin-top: 50px;
+  box-shadow: 0px 0px 7px rgba(100, 96, 106, 0.2);
+`
+
+const MainImage= styled.div`
+  width: 50%;
+  background-image: url(${gym});
+  background-size: cover;
+  background-position: center;
+  border-radius: 0 20px 20px 0px;
+`
+
+const Registration = styled.div`
+  width: 50%;
+  padding-top: 50px;
+  padding-left: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  margin-bottom: 15px;
+`
+
+const Divider = styled.div`
+    width: 85%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 25px;
+    margin-bottom: 20px;
+
+    & span {
+    font-weight: 500;
+    }
+`
+
+const Line = styled.div`
+    width: 32%;
+    height: 1px;
+    background-color: silver;
+`
+
+const Footer = styled.footer`
+  margin-bottom: 30px;
+`
+
+const Special = styled.a`
+      border-bottom: 2px solid #413fb5;
+    text-decoration: none;
+    color: black;
+    font-weight: 600;
+`
